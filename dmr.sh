0085345b163eb24714e466d77423cf06637ca9dd
sillda76 <sillda76@gmail.com>
2025-03-03 06:09:31 +0800

    Update dmr.sh

--- dmr.sh
+++ dmr.sh
@@ -699,17 +699,16 @@ main_menu() {
     while true; do
         show_header
         show_status
-        echo -e "${CYAN}${BOLD}请选择操作：${NC}${NORMAL}"
         echo -e "${BLUE}${BOLD}1.${NC}${NORMAL} 安装DanmakuRender v5"
         echo -e "${BLUE}${BOLD}2.${NC}${NORMAL} 启动/停止录制"
-        echo -e "${BLUE}${BOLD}3.${NC}${NORMAL} 查看实时日志"
+        echo -e "${BLUE}${BOLD}3.${NC}${NORMAL} 查看实时日志(按Q退出查看)"
         echo -e "${BLUE}${BOLD}4.${NC}${NORMAL} 手动渲染视频"
         echo -e "${BLUE}${BOLD}5.${NC}${NORMAL} 运行一次测试"
         echo -e "${BLUE}${BOLD}6.${NC}${NORMAL} 删除回放/渲染视频文件"
-        echo -e "${BLUE}${BOLD}7.${NC}${NORMAL} biliup-rs工具"
+        echo -e "${BLUE}${BOLD}7.${NC}${NORMAL} biliup-r工具"
         echo -e "${BLUE}${BOLD}8.${NC}${NORMAL} 安装微软雅黑和Emoji表情"
         echo -e "${BLUE}${BOLD}9.${NC}${NORMAL} 安装阿里巴巴普惠体和Emoji表情"
-        echo -e "${BLUE}${BOLD}9.${NC}${NORMAL} ${LIGHT_BLUE}更新DanmakuRender v5"
+        echo -e "${BLUE}${BOLD}10.${NC}${NORMAL}${LIGHT_BLUE}更新DanmakuRender v5"
         echo -e "${BLUE}${BOLD}11.${NC}${NORMAL}${RED}${BOLD}卸载DanmakuRender v5"
         echo -e "${BLUE}${BOLD}0.${NC}${NORMAL} 退出脚本"
         
