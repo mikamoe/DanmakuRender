30673c74feaa2f0fecf79862b8350d19998f6fe2
sillda76 <sillda76@gmail.com>
2025-04-06 20:52:02 +0800

    Update dmr.sh

--- dmr.sh
+++ dmr.sh
@@ -753,14 +753,9 @@ biliup_append() {
         fi
     fi
 
-    read -p "请输入tid号（默认65）: " tid
-    tid=${tid:-65}
-    read -p "请输入视频标签（默认直播回放,录播）: " tags
-    tags=${tags:-"直播回放,录播"}
-
     cd "$BILIUP_DIR" || { echo -e "${RED}${BOLD}[ERROR]${NC}${NORMAL} 进入工具目录失败！"; return 1; }
-    echo -e "${BLUE}${BOLD}[INFO]${NC}${NORMAL} ${BLUE}执行命令：./biliup upload ${video_paths[*]} --tid $tid --tag \"$tags\"${NC}"
-    ./biliup upload "${video_paths[@]}" --tid "$tid" --tag "$tags"
+    echo -e "${BLUE}${BOLD}[INFO]${NC}${NORMAL} ${BLUE}执行命令：./biliup append --vid $bv ${video_paths[*]}${NC}"
+    ./biliup append --vid "$bv" "${video_paths[@]}"
 }
 
 # 哔哩哔哩工具子菜单：展示版本信息及相关上传、登录、更新选项
