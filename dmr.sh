b9182bc828d4701e086b4f2b13ef2f7c99366339
sillda76 <sillda76@gmail.com>
2025-07-13 04:38:22 +0800

    Update dmr.sh

--- dmr.sh
+++ dmr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 版本号
-VERSION="2025-07-13 D"
+VERSION="2025-07-13 E"
 
 # ===================== 配置变量 =====================
 # 安装路径及相关文件、目录设置
@@ -868,9 +868,9 @@ delete_replays() {
                 local fsize=$(du -h "$filepath" | awk '{print $1}')
                 local index=$((i + 1))
                 if [ $index -lt 10 ]; then
-                    printf "  %d) ${CYAN}%s${NC} ${GREEN}(%s)${NC}\n" "$index" "$fname" "$fsize"
+                    printf " %2d) ${CYAN}%s${NC} ${GREEN}(%s)${NC}\n" "$index" "$fname" "$fsize"
                 else
-                    printf "%d) ${CYAN}%s${NC} ${GREEN}(%s)${NC}\n" "$index" "$fname" "$fsize"
+                    printf "%2d) ${CYAN}%s${NC} ${GREEN}(%s)${NC}\n" "$index" "$fname" "$fsize"
                 fi
             done
         fi
