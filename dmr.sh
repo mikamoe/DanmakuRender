55ec3cee3ef99772a764a44be108580f6919917a
sillda76 <sillda76@gmail.com>
2025-07-16 02:08:01 +0800

    Update dmr.sh

--- dmr.sh
+++ dmr.sh
@@ -192,15 +192,15 @@ fetch_github_times() {
 
     if [ -n "$local_ver" ] && [ -n "$remote_ver" ] && [ "$local_ver" != "$remote_ver" ]; then
         echo -e "${YELLOW}${BOLD}检测到项目有新版本！建议运行选项 10 进行更新。${NC}"
-        echo -e "^_^ 发现新版本啦！"
+        echo -e "又又又又又发现新版本啦！"
         echo -e "${ORANGE}${BOLD}本地版本: ${PINK}${BOLD}${local_ver}${NC}"
         echo -e "${YELLOW}${BOLD}远程版本: ${CYAN}${BOLD}${remote_ver}${NC}"
         echo -e "${BOLD}提交说明: ${CYAN}${commit_message}${NC}"
 
         if [ -n "$commit_sha" ]; then
-            echo -e "更新详情: ${BLUE}${DMR_GITHUB_BASE}/commit/${commit_sha}${NC}"
+            echo -e "${BOLD}更新详情: ${BLUE}${DMR_GITHUB_BASE}/commit/${commit_sha}${NC}"
         else
-            echo -e "更新详情 (分支): ${BLUE}${DMR_GITHUB_BASE}/commits/${GITHUB_BRANCH}${NC}"
+            echo -e "${BOLD}更新详情 (分支): ${BLUE}${DMR_GITHUB_BASE}/commits/${GITHUB_BRANCH}${NC}"
         fi
 
         echo -e "按任意键继续..."
